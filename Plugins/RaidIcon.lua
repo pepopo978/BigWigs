c328980318a81100c47b44242112903398f49c5d
pepopo978 <pepopo978@gmail.com>
2024-03-27 10:37:07 -0700

    revert raidicon changes

--- Plugins/RaidIcon.lua
+++ Plugins/RaidIcon.lua
@@ -11,200 +11,202 @@ local lastplayer = nil
 --      Localization      --
 ----------------------------
 
-L:RegisterTranslations("enUS", function() return {
-	["Raid Icons"] = true,
+L:RegisterTranslations("enUS", function()
+	return {
+		["Raid Icons"] = true,
 
-	["raidicon"] = true,
-	["place"] = true,
-	["icon"] = true,
-	["sticky"] = true,
-	["chat"] = true,
+		["raidicon"] = true,
+		["place"] = true,
+		["icon"] = true,
 
-	["Place"] = true,
-	["Place Raid Icons"] = true,
-	["Toggle placing of Raid Icons on players."] = true,
+		["Place"] = true,
+		["Place Raid Icons"] = true,
+		["Toggle placing of Raid Icons on players."] = true,
 
-	["Icon"] = true,
-	["Set Icon"] = true,
-	["Set which icon to place on players."] = true,
+		["Icon"] = true,
+		["Set Icon"] = true,
+		["Set which icon to place on players."] = true,
 
-	["Sticky"] = true,
-	["Sticky Markers"] = true,
-	["Markers are not removed by reapplying the same mark on a unit."] = true,
+		["Options for Raid Icons."] = true,
 
-	["Chat"] = true,
-	["Chat Markers"] = true,
-	["Type {marker} to post it in chat channels."] = true,
+		["star"] = true,
+		["circle"] = true,
+		["diamond"] = true,
+		["triangle"] = true,
+		["moon"] = true,
+		["square"] = true,
+		["cross"] = true,
+		["skull"] = true,
+	}
+end)
 
-	["Options for Raid Icons."] = true,
+L:RegisterTranslations("esES", function()
+	return {
+		["Raid Icons"] = "Iconos de Banda",
 
-	["star"] = true,
-	["circle"] = true,
-	["diamond"] = true,
-	["triangle"] = true,
-	["moon"] = true,
-	["square"] = true,
-	["cross"] = true,
-	["skull"] = true,
-} end )
+		--["raidicon"] = "iconobanda",
+		--["place"] = "colocar",
+		--["icon"] = "icono",
 
-L:RegisterTranslations("esES", function() return {
-	["Raid Icons"] = "Iconos de Banda",
+		["Place"] = "Marcar",
+		["Place Raid Icons"] = "Marcar con los iconos de banda",
+		["Toggle placing of Raid Icons on players."] = "Alterna marcar los Iconos de Banda a los jugadores",
 
-	--["raidicon"] = "iconobanda",
-	--["place"] = "colocar",
-	--["icon"] = "icono",
+		["Icon"] = "Icono",
+		["Set Icon"] = "Definir Icono",
+		["Set which icon to place on players."] = "Define cual icono que marcar al jugador",
 
-	["Place"] = "Marcar",
-	["Place Raid Icons"] = "Marcar con los iconos de banda",
-	["Toggle placing of Raid Icons on players."] = "Alterna marcar los Iconos de Banda a los jugadores",
+		["Options for Raid Icons."] = "Opciones para Iconos de Banda",
 
-	["Icon"] = "Icono",
-	["Set Icon"] = "Definir Icono",
-	["Set which icon to place on players."] = "Define cual icono que marcar al jugador",
+		["star"] = "estrella",
+		["circle"] = "círculo",
+		["diamond"] = "diamante",
+		["triangle"] = "triángulo",
+		["moon"] = "luna",
+		["square"] = "cuadrado",
+		["cross"] = "cruz",
+		["skull"] = "calavera",
+	}
+end)
 
-	["Options for Raid Icons."] = "Opciones para Iconos de Banda",
+L:RegisterTranslations("koKR", function()
+	return {
+		["Raid Icons"] = "공격대 아이콘",
 
-	["star"] = "estrella",
-	["circle"] = "círculo",
-	["diamond"] = "diamante",
-	["triangle"] = "triángulo",
-	["moon"] = "luna",
-	["square"] = "cuadrado",
-	["cross"] = "cruz",
-	["skull"] = "calavera",
-} end )
+		["Place"] = "지정",
+		["Place Raid Icons"] = "공격대 아이콘 지정",
+		["Toggle placing of Raid Icons on players."] = "플레이어에 공격대 아이콘 지정 토글",
 
-L:RegisterTranslations("koKR", function() return {
-	["Raid Icons"] = "공격대 아이콘",
+		["Icon"] = "아이콘",
+		["Set Icon"] = "아이콘 설정",
+		["Set which icon to place on players."] = "플레이어에 아이콘 지정을 위한 설정",
 
-	["Place"] = "지정",
-	["Place Raid Icons"] = "공격대 아이콘 지정",
-	["Toggle placing of Raid Icons on players."] = "플레이어에 공격대 아이콘 지정 토글",
+		["Options for Raid Icons."] = "공격대 아이콘에 대한 설정",
 
-	["Icon"] = "아이콘",
-	["Set Icon"] = "아이콘 설정",
-	["Set which icon to place on players."] = "플레이어에 아이콘 지정을 위한 설정",
+		["star"] = "별",
+		["circle"] = "원",
+		["diamond"] = "다이아몬드",
+		["triangle"] = "세모",
+		["moon"] = "달",
+		["square"] = "네모",
+		["cross"] = "가위표",
+		["skull"] = "해골",
 
-	["Options for Raid Icons."] = "공격대 아이콘에 대한 설정",
+	}
+end)
 
-	["star"] = "별",
-	["circle"] = "원",
-	["diamond"] = "다이아몬드",
-	["triangle"] = "세모",
-	["moon"] = "달",
-	["square"] = "네모",
-	["cross"] = "가위표",
-	["skull"] = "해골",
+L:RegisterTranslations("zhCN", function()
+	return {
+		["Raid Icons"] = "团队图标",
 
-} end )
+		["Place"] = "标记",
+		["Place Raid Icons"] = "标记团队图标",
+		["Toggle placing of Raid Icons on players."] = "切换是否在玩家身上标记团队图标",
 
-L:RegisterTranslations("zhCN", function() return {
-	["Raid Icons"] = "团队图标",
+		["Icon"] = "图标",
+		["Set Icon"] = "设置图标",
+		["Set which icon to place on players."] = "设置玩家身上标记的图标。",
 
-	["Place"] = "标记",
-	["Place Raid Icons"] = "标记团队图标",
-	["Toggle placing of Raid Icons on players."] = "切换是否在玩家身上标记团队图标",
+		["Options for Raid Icons."] = "团队图标设置",
 
-	["Icon"] = "图标",
-	["Set Icon"] = "设置图标",
-	["Set which icon to place on players."] = "设置玩家身上标记的图标。",
+		["star"] = "星星",
+		["circle"] = "圆圈",
+		["diamond"] = "钻石",
+		["triangle"] = "三角",
+		["moon"] = "月亮",
+		["square"] = "方形",
+		["cross"] = "十字",
+		["skull"] = "骷髅",
+	}
+end)
 
-	["Options for Raid Icons."] = "团队图标设置",
+L:RegisterTranslations("zhTW", function()
+	return {
+		["Raid Icons"] = "團隊圖示",
 
-	["star"] = "星星",
-	["circle"] = "圆圈",
-	["diamond"] = "钻石",
-	["triangle"] = "三角",
-	["moon"] = "月亮",
-	["square"] = "方形",
-	["cross"] = "十字",
-	["skull"] = "骷髅",
-} end )
+		["Place"] = "標記",
+		["Place Raid Icons"] = "標記團隊圖示",
+		["Toggle placing of Raid Icons on players."] = "切換是否在玩家身上標記團隊圖示",
 
-L:RegisterTranslations("zhTW", function() return {
-	["Raid Icons"] = "團隊圖示",
+		["Icon"] = "圖標",
+		["Set Icon"] = "設置圖示",
+		["Set which icon to place on players."] = "設置玩家身上標記的圖示。",
 
-	["Place"] = "標記",
-	["Place Raid Icons"] = "標記團隊圖示",
-	["Toggle placing of Raid Icons on players."] = "切換是否在玩家身上標記團隊圖示",
+		["Options for Raid Icons."] = "團隊圖示設置",
 
-	["Icon"] = "圖標",
-	["Set Icon"] = "設置圖示",
-	["Set which icon to place on players."] = "設置玩家身上標記的圖示。",
+		["star"] = "星星",
+		["circle"] = "圓圈",
+		["diamond"] = "方塊",
+		["triangle"] = "三角",
+		["moon"] = "月亮",
+		["square"] = "方形",
+		["cross"] = "十字",
+		["skull"] = "骷髏",
+	}
+end)
 
-	["Options for Raid Icons."] = "團隊圖示設置",
+L:RegisterTranslations("deDE", function()
+	return {
+		["Raid Icons"] = "Schlachtzug-Symbole",
 
-	["star"] = "星星",
-	["circle"] = "圓圈",
-	["diamond"] = "方塊",
-	["triangle"] = "三角",
-	["moon"] = "月亮",
-	["square"] = "方形",
-	["cross"] = "十字",
-	["skull"] = "骷髏",
-} end )
+		--["raidicon"] = "schlachtzugsymbol",
+		--["place"] = "position",
+		--["icon"] = "symbol",
 
-L:RegisterTranslations("deDE", function() return {
-	["Raid Icons"] = "Schlachtzug-Symbole",
+		["Place"] = "Platzierung",
+		["Place Raid Icons"] = "Schlachtzug-Symbole platzieren",
+		["Toggle placing of Raid Icons on players."] = "Schlachtzug-Symbole auf Spieler setzen.",
 
-	--["raidicon"] = "schlachtzugsymbol",
-	--["place"] = "position",
-	--["icon"] = "symbol",
+		["Icon"] = "Symbol",
+		["Set Icon"] = "Symbol platzieren",
+		["Set which icon to place on players."] = "W\195\164hle, welches Symbol auf Spieler gesetzt wird.",
 
-	["Place"] = "Platzierung",
-	["Place Raid Icons"] = "Schlachtzug-Symbole platzieren",
-	["Toggle placing of Raid Icons on players."] = "Schlachtzug-Symbole auf Spieler setzen.",
+		["Options for Raid Icons."] = "Optionen f\195\188r Schlachtzug-Symbole.",
 
-	["Icon"] = "Symbol",
-	["Set Icon"] = "Symbol platzieren",
-	["Set which icon to place on players."] = "W\195\164hle, welches Symbol auf Spieler gesetzt wird.",
+		["star"] = "Stern",
+		["circle"] = "Kreis",
+		["diamond"] = "Diamant",
+		["triangle"] = "Dreieck",
+		["moon"] = "Mond",
+		["square"] = "Quadrat",
+		["cross"] = "Kreuz",
+		["skull"] = "Totenkopf",
+	}
+end)
 
-	["Options for Raid Icons."] = "Optionen f\195\188r Schlachtzug-Symbole.",
+L:RegisterTranslations("frFR", function()
+	return {
+		["Raid Icons"] = "Icônes de raid",
 
-	["star"] = "Stern",
-	["circle"] = "Kreis",
-	["diamond"] = "Diamant",
-	["triangle"] = "Dreieck",
-	["moon"] = "Mond",
-	["square"] = "Quadrat",
-	["cross"] = "Kreuz",
-	["skull"] = "Totenkopf",
-} end )
+		["Place"] = "Placement",
+		["Place Raid Icons"] = "Placer les icônes de raid",
+		["Toggle placing of Raid Icons on players."] = "Place ou non les icônes de raid sur les joueurs.",
 
-L:RegisterTranslations("frFR", function() return {
-	["Raid Icons"] = "Icônes de raid",
+		["Icon"] = "Icône",
+		["Set Icon"] = "Déterminer l'icône",
+		["Set which icon to place on players."] = "Détermine quelle icône sera placée sur les joueurs.",
 
-	["Place"] = "Placement",
-	["Place Raid Icons"] = "Placer les icônes de raid",
-	["Toggle placing of Raid Icons on players."] = "Place ou non les icônes de raid sur les joueurs.",
+		["Options for Raid Icons."] = "Options concernant les icônes de raid.",
 
-	["Icon"] = "Icône",
-	["Set Icon"] = "Déterminer l'icône",
-	["Set which icon to place on players."] = "Détermine quelle icône sera placée sur les joueurs.",
-
-	["Options for Raid Icons."] = "Options concernant les icônes de raid.",
-
-	["star"] = "étoile",
-	["circle"] = "cercle",
-	["diamond"] = "diamant",
-	["triangle"] = "triangle",
-	["moon"] = "lune",
-	["square"] = "carré",
-	["cross"] = "croix",
-	["skull"] = "crâne",
-} end )
+		["star"] = "étoile",
+		["circle"] = "cercle",
+		["diamond"] = "diamant",
+		["triangle"] = "triangle",
+		["moon"] = "lune",
+		["square"] = "carré",
+		["cross"] = "croix",
+		["skull"] = "crâne",
+	}
+end)
 
 ----------------------------------
 --      Module Declaration      --
 ----------------------------------
 
-BigWigsRaidIcon = BigWigs:NewModule(L["Raid Icons"], "AceHook-2.1")
+BigWigsRaidIcon = BigWigs:NewModule(L["Raid Icons"])
 BigWigsRaidIcon.defaultDB = {
 	place = true,
 	icon = L["skull"],
-	sticky = true,
-	chat = true,
 }
 BigWigsRaidIcon.icontonumber = {
 	[L["star"]] = 1,
@@ -221,35 +223,29 @@ BigWigsRaidIcon.consoleOptions = {
 	type = "group",
 	name = L["Raid Icons"],
 	desc = L["Options for Raid Icons."],
-	args   = {
+	args = {
 		[L["place"]] = {
 			type = "toggle",
 			name = L["Place Raid Icons"],
 			desc = L["Toggle placing of Raid Icons on players."],
-			get = function() return BigWigsRaidIcon.db.profile.place end,
-			set = function(v) BigWigsRaidIcon.db.profile.place = v end,
+			get = function()
+				return BigWigsRaidIcon.db.profile.place
+			end,
+			set = function(v)
+				BigWigsRaidIcon.db.profile.place = v
+			end,
 		},
 		[L["icon"]] = {
 			type = "text",
 			name = L["Set Icon"],
 			desc = L["Set which icon to place on players."],
-			get = function() return BigWigsRaidIcon.db.profile.icon end,
-			set = function(v) BigWigsRaidIcon.db.profile.icon = v end,
-			validate = {L["star"], L["circle"], L["diamond"], L["triangle"], L["moon"], L["square"], L["cross"], L["skull"] },
-		},
-		[L["sticky"]] = {
-			type = "toggle",
-			name = L["Sticky Markers"],
-			desc = L["Markers are not removed by reapplying the same mark on a unit."],
-			get = function() return BigWigsRaidIcon.db.profile.sticky end,
-			set = function(v) BigWigsRaidIcon.db.profile.sticky = v end,
-		},
-		[L["chat"]] = {
-			type = "toggle",
-			name = L["Chat Markers"],
-			desc = L["Type {marker} to post it in chat channels."],
-			get = function() return BigWigsRaidIcon.db.profile.chat end,
-			set = function(v) BigWigsRaidIcon.db.profile.chat = v end,
+			get = function()
+				return BigWigsRaidIcon.db.profile.icon
+			end,
+			set = function(v)
+				BigWigsRaidIcon.db.profile.icon = v
+			end,
+			validate = { L["star"], L["circle"], L["diamond"], L["triangle"], L["moon"], L["square"], L["cross"], L["skull"] },
 		},
 	}
 }
@@ -261,25 +257,25 @@ BigWigsRaidIcon.consoleOptions = {
 function BigWigsRaidIcon:OnEnable()
 	self:RegisterEvent("BigWigs_SetRaidIcon")
 	self:RegisterEvent("BigWigs_RemoveRaidIcon")
-	self:Hook("SetRaidTargetIcon", "StickySetRaidTarget")
-	self:Hook("SendChatMessage", "ConvertRaidMarkers")
 end
 
 function BigWigsRaidIcon:BigWigs_SetRaidIcon(player, iconnumber)
-	if not self.db.profile.place or not player then return end
+	if not self.db.profile.place or not player then
+		return
+	end
 	local icon = self.db.profile.icon
 	if not self.icontonumber[icon] then
 		icon = L["skull"]
 	end
 	icon = self.icontonumber[icon]
 
-	for i=1,GetNumRaidMembers() do
-		if UnitName("raid"..i) == player then
+	for i = 1, GetNumRaidMembers() do
+		if UnitName("raid" .. i) == player then
 			if not iconnumber then
-				SetRaidTarget("raid"..i, icon)
+				SetRaidTargetIcon("raid" .. i, icon)
 				lastplayer = player
 			else
-				SetRaidTarget("raid"..i, iconnumber)
+				SetRaidTargetIcon("raid" .. i, iconnumber)
 				lastplayer = player
 			end
 		end
@@ -288,32 +284,13 @@ function BigWigsRaidIcon:BigWigs_SetRaidIcon(player, iconnumber)
 end
 
 function BigWigsRaidIcon:BigWigs_RemoveRaidIcon()
-	if not self.db.profile.place or not lastplayer then return end
-	for i=1,GetNumRaidMembers() do
-		if UnitName("raid"..i) == lastplayer then
-			SetRaidTarget("raid"..i, 0)
+	if not self.db.profile.place or not lastplayer then
+		return
+	end
+	for i = 1, GetNumRaidMembers() do
+		if UnitName("raid" .. i) == lastplayer then
+			SetRaidTargetIcon("raid" .. i, 0)
 		end
 	end
 	lastplayer = nil
-end
-
-function BigWigsRaidIcon:StickySetRaidTarget(unit, index)
-	self.hooks["SetRaidTargetIcon"](unit, index)
-	if self.db.profile.sticky then
-		SetRaidTarget(unit, index);
-	end
-end
-
-function BigWigsRaidIcon:ConvertRaidMarkers(msg, chatType, language, channel)
-	if self.db.profile.chat then
-		msg = string.gsub(msg, "{star}", SpellstatusV2IndexToIcon[1])
-		msg = string.gsub(msg, "{circle}", SpellstatusV2IndexToIcon[2])
-		msg = string.gsub(msg, "{diamond}", SpellstatusV2IndexToIcon[3])
-		msg = string.gsub(msg, "{triangle}", SpellstatusV2IndexToIcon[4])
-		msg = string.gsub(msg, "{moon}", SpellstatusV2IndexToIcon[5])
-		msg = string.gsub(msg, "{square}", SpellstatusV2IndexToIcon[6])
-		msg = string.gsub(msg, "{cross}", SpellstatusV2IndexToIcon[7])
-		msg = string.gsub(msg, "{skull}", SpellstatusV2IndexToIcon[8])
-	end
-	self.hooks["SendChatMessage"](msg, chatType, language, channel)
 end
